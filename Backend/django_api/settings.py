5ce479e9ab59baa0bd59674cc358a6b8cb5ff42e
fenyrmaster <brandony2004@gmail.com>
2026-01-04 18:19:07 -0600

    Contenedor completamente transferido para hacer posible su uso con docker

--- Backend/django_api/settings.py
+++ Backend/django_api/settings.py
@@ -100,23 +100,23 @@ WSGI_APPLICATION = 'django_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE'),
-        'USER': os.environ.get('MYSQL_USER'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
-        'HOST': os.environ.get('MYSQL_HOST'),
-        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+        'NAME': 'pavlovadb',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': 3306,
     }
-}"""
+}
 
 # Storage settings for Azure Blob Storage
 
